9b638db010af6fe79251072698d3ee0eb086c795
Nayoung Kim <nayoung.kimpak@gmail.com>
2020-04-24 13:37:16 -0500

    feat: create new npm project

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const $ = require('jquery');
+
+
+console.log('hello');
+
+const person = {
+    firstname: 'nayoung',
+};
+
+const {firstname} = person;
+
+const sayHello = (person) =>(`hello ${person.firstname}. `);
+console.log(sayHello(person));
+sayHello(person);
+
+$(document).ready(() =>{
+    $('body').html(sayHello(person));
+});
